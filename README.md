28447581acd508f5a7f2c5629758a572affab6a9
Austin Wayman <awguitar@msn.com>
2018-11-09 14:20:18 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Theofficefriendfinder
+Use our survery to see which character from the Office would be your best friend!
+
+I did not stylize as much as I would like unfortunately. 
---
+++ data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        name: "Michael Scott", 
+        photo: "https://media.giphy.com/media/wQzqIYHE15zMI/giphy.gif",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "2",
+            "5",
+            "4",
+            "5",
+            "1",
+            "5",
+            "3",
+        ]
+    },
+    {
+        name: "Jim Halpert", 
+        photo: "https://media.giphy.com/media/13jghlUIB6FHZm/giphy.gif",
+        scores: [
+            "5",
+            "2",
+            "3",
+            "1",
+            "4",
+            "2",
+            "2",
+            "4",
+            "2",
+            "5",
+        ]
+    },
+    {
+        name: "Dwight Schrute", 
+        photo: "https://media.giphy.com/media/OLHy9ERaFUvzW/giphy.gif",
+        scores: [
+            "2",
+            "2",
+            "5",
+            "3",
+            "5",
+            "5",
+            "1",
+            "3",
+            "4",
+            "2",
+        ]
+    },
+    {
+        name: "Kevin Malone", 
+        photo: "https://media.giphy.com/media/Su7qfpu8YVBqE/giphy.gif",
+        scores: [
+            "3",
+            "1",
+            "2",
+            "5",
+            "1",
+            "3",
+            "5",
+            "2",
+            "3",
+            "2",
+        ]
+    },
+    {
+        name: "Pam Beasley", 
+        photo: "https://media.giphy.com/media/vfi3QZdmwsiQ0/giphy.gif",
+        scores: [
+            "3",
+            "5",
+            "2",
+            "1",
+            "2",
+            "1",
+            "5",
+            "4",
+            "2",
+            "5",
+        ]
+    },
+];
+
+module.exports = friends;
